7ea3bc5412e30be07041b3db5c8b87142fa3146e
Churchill <churchillrimamskep@gmail.com>
2025-02-20 16:59:09 +0100

    added something

--- resource/src/app.module.ts
+++ resource/src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, // Make the ConfigModule global
-      envFilePath: '.env', // Specify the path to your .env file
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
