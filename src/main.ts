1ea7376ade46fc6559193ec829f42a7639151092
Churchill Daniel <churchillrimamskep@gmail.com>
2025-04-19 18:06:49 +0100

    feat(validation): update deleteRoomValidation to use CUID format for ID

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
   const limiter = rateLimit({
-    max: 100, // 100 requests
+    max: 10000, // 100 requests
     windowMs: 60 * 60 * 1000, // 1 hour
     message:
       'We have received too many requests from this IP. Please try again after one hour.',
